4cc037879608e077f3e14924067dbe1bdc8effb6
bernardo1411 <bernardo1411@hotmail.com>
2020-06-10 14:43:45 -0300

    Update

--- queuefood/src/App.js
+++ queuefood/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import Main from './components/main'
+import About from './components/About'
+import Contact from './components/Contact'
+import { Router, Switch } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <Switch>
+          <h1>Hello World!</h1>
+          <Router exact to='/' component={Main}/>
+          <Router to='/about' component={About}/>
+          <Router to='/contact' component={Contact}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
