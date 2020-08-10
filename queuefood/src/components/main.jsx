5f846d77c4d8e806bbee8466c5ae669095c9eba5
bernardo1411 <bernardo1411@hotmail.com>
2020-08-10 10:26:58 -0300

    Update main.jsx

--- queuefood/src/components/main.jsx
+++ queuefood/src/components/main.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 import ListCard from './cards/listCards'
@@ -10,8 +10,8 @@ import ListCard from './cards/listCards'
 toast.configure()
 class Main extends Component {
 
-    notify = (isAuth) =>{
-        isAuth ? toast.success('Product Added', {position: toast.POSITION.TOP_CENTER, autoClose: 2000, hideProgressBar: true}) : toast.error('Login first!', {position: toast.POSITION.TOP_CENTER, autoClose: 2000, hideProgressBar: true})
+    notify = (isAuth) => {
+        isAuth ? toast.success('Product Added', { position: toast.POSITION.TOP_CENTER, autoClose: 2000, hideProgressBar: true }) : toast.error('Login first!', { position: toast.POSITION.TOP_CENTER, autoClose: 2000, hideProgressBar: true })
     }
 
     render() {
@@ -22,10 +22,10 @@ class Main extends Component {
                 <div className="bg-dark d-flex justify-content-center">
                     <h2 className="display-5 text-light">Menu</h2>
                 </div>
-                {Array.isArray(listStore) && listStore.length !== 0 ? listStore.map(store =>{
-                    return <ListCard store={store} notify={this.notify} key={store.id}/>
-                }):null
-            }
+                {Array.isArray(listStore) && listStore.length !== 0 ? listStore.map(store => {
+                    return <ListCard store={store} notify={this.notify} key={store.id} />
+                }) : null
+                }
             </div>
         )
     }
